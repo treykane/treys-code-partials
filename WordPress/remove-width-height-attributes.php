24158341da6d5592124e3076cdc8b2fe31379f1d
Trey <treykane@gmail.com>
2015-08-20 11:22:26 -0400

    Wordpress: Remove auto-inserted height and width attributes on images.

---
+++ WordPress/remove-width-height-attributes.php
@@ -0,0 +1,33 @@
+<?php
+// This will remove the height and width attributes automattically applied to images inserted from the WordPress media library. Though this may not be the best way to approach loading images, it will allow you flexibility when you need to style dynamic content images using CSS. Source: http://wordpress.stackexchange.com/questions/29881/stop-wordpress-from-hardcoding-img-width-and-height-attributes
+
+function myprefix_image_downsize( $value = false, $id, $size ) {
+    if ( !wp_attachment_is_image($id) )
+        return false;
+
+    $img_url = wp_get_attachment_url($id);
+    $is_intermediate = false;
+    $img_url_basename = wp_basename($img_url);
+
+    // try for a new style intermediate size
+    if ( $intermediate = image_get_intermediate_size($id, $size) ) {
+        $img_url = str_replace($img_url_basename, $intermediate['file'], $img_url);
+        $is_intermediate = true;
+    }
+    elseif ( $size == 'thumbnail' ) {
+        // Fall back to the old thumbnail
+        if ( ($thumb_file = wp_get_attachment_thumb_file($id)) && $info = getimagesize($thumb_file) ) {
+            $img_url = str_replace($img_url_basename, wp_basename($thumb_file), $img_url);
+            $is_intermediate = true;
+        }
+    }
+
+    // We have the actual image size, but might need to further constrain it if content_width is narrower
+    if ( $img_url) {
+        return array( $img_url, 0, 0, $is_intermediate );
+    }
+    return false;
+}
+add_filter( 'image_downsize', 'myprefix_image_downsize', 1, 3 );
+
+?>
